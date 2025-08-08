477aba687c342c0c74e142b695252a754b686fa3
whokartikgambhir <gambhirkartik6@gmail.com>
2025-08-08 23:37:28 +0530

    BPP-Implemented-dbsync-api-for-mongo-operations

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes.js';
+import internalRoute from "./routes/internalRoute.js";
 import { config } from './config.js';
 
 const app = express();
 
 app.use(express.json());
+app.use("/internal", internalRoute);
 app.use('/api/auth', authRoutes);
 
 const PORT = config.port || 5000;
